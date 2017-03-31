9d415da281e26466d2bc7326c4cd1bd0c85ee922
liviunicu <liviu-alexandru.nicu@my.fmi.unibuc.ro>
2017-04-01 00:38:24 +0300

    routes

--- node/routes.js
+++ node/routes.js
@@ -3,7 +3,7 @@ var express=require('express');
 
 module.exports=function(app){
     var user=require('./controllers/userNodeController.js');
-    
+
 	app.post('/user/login/user', user.login);
     app.post('/user/register/user', user.register);
     app.post('/user/getAllUsers',user.getAllUsers);
